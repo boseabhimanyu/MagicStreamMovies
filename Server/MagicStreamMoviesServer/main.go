55b2ea22e0841a47a02a9644c6ee795daf72984c
Abhimanyu Bose <boseabhimanyu@gmail.com>
2026-06-11 23:09:15 +0530

    React  and cors setup
    React installation and setup. Displaying Movies from db. Installation and  cors module and setup in main.go

--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/boseabhimanyu/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
 	"github.com/boseabhimanyu/MagicStreamMovies/Server/MagicStreamMoviesServer/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -36,6 +38,17 @@ func main() {
 			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
 		}
 	}()
+
+	config := cors.Config{}
+
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PATCH"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(config))
+	router.Use(gin.Logger())
 
 	routes.SetupUnProtectedRoutes(router, client)
 	routes.SetupProtectedRoutes(router, client)
