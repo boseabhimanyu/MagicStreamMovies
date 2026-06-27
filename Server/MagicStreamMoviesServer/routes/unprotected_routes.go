7d5ec47667ea20ef33a42413b7f1c3df4bd61011
Abhimanyu Bose <boseabhimanyu@gmail.com>
2026-06-27 10:28:34 +0530

    Login page setup and get genres
    Login and register page setup. Get genres functionality. Adding navigation bar in client

--- Server/MagicStreamMoviesServer/routes/unprotected_routes.go
+++ Server/MagicStreamMoviesServer/routes/unprotected_routes.go
@@ -10,4 +10,5 @@ func SetupUnProtectedRoutes(router *gin.Engine, client *mongo.Client) {
 	router.GET("/movies", controllers.GetMovies(client))
 	router.POST("/register", controllers.RegisterUser(client))
 	router.POST("/login", controllers.LoginUser(client))
+	router.GET("/genres", controllers.GetGenres(client))
 }
